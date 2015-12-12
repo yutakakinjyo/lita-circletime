6a5960ed4c6d52ac51917a1dcaf7f4f9c1a07558
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-12 13:56:49 +0900

    add route

--- lib/lita/handlers/circletime.rb
+++ lib/lita/handlers/circletime.rb
@@ -3,6 +3,11 @@ module Lita
     class Circletime < Handler
       # insert handler code here
 
+      route(/^build today/, :today)
+      route(/^build yestaday/, :yestaday)
+      route(/^build week/, :week)
+      route(/^build month/, :month)
+
       Lita.register_handler(self)
     end
   end
--- spec/lita/handlers/circletime_spec.rb
+++ spec/lita/handlers/circletime_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helper"
 
 describe Lita::Handlers::Circletime, lita_handler: true do
+
+  it { is_expected.to route("build today").to(:today) }
+  it { is_expected.to route("build yestaday").to(:yestaday) }
+  it { is_expected.to route("build week").to(:week) }
+  it { is_expected.to route("build month").to(:month) }
+
 end
