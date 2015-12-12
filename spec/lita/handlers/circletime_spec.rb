3614ae2c76d2d475e0a9c43378f0d555afe71431
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-12 16:23:11 +0900

    imple routes

--- spec/lita/handlers/circletime_spec.rb
+++ spec/lita/handlers/circletime_spec.rb
@@ -2,10 +2,15 @@ require "spec_helper"
 
 describe Lita::Handlers::Circletime, lita_handler: true do
 
-  it { is_expected.to route("build\s+(+*)").to(:set) }
+  it { is_expected.to route("org orgname").to(:set) }
   it { is_expected.to route("build today").to(:today) }
   it { is_expected.to route("build yestaday").to(:yestaday) }
   it { is_expected.to route("build week").to(:week) }
   it { is_expected.to route("build month").to(:month) }
+  
+  it "set" do
+    send_message("org yutakakinjyo")
+    expect(replies.last).to eq("set yutakakinjyo to org name")
+  end
 
 end
