0817dedad17f41314805c3d5503ac1b5d6c51d9a
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-13 00:38:23 +0900

    add show command

--- spec/lita/handlers/circletime_spec.rb
+++ spec/lita/handlers/circletime_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe Lita::Handlers::Circletime, lita_handler: true do
 
   it { is_expected.to route("org orgname").to(:set) }
+  it { is_expected.to route("org").to(:show) }
   it { is_expected.to route("build today").to(:today) }
   it { is_expected.to route("build yestaday").to(:yestaday) }
   it { is_expected.to route("build week").to(:week) }
@@ -12,5 +13,18 @@ describe Lita::Handlers::Circletime, lita_handler: true do
     send_message("org yutakakinjyo")
     expect(replies.last).to eq("set yutakakinjyo to org name")
   end
+
+  it "show org name" do
+    send_message("org yutakakinjyo")
+    send_message("org")
+    expect(replies.last).to eq("yutakakinjyo is setted to org now")
+  end
+
+  it "show org name" do
+    send_message("org")
+    expect(replies.last).to eq("you not set org name yet")
+  end
+
+
 
 end
